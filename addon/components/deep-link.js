cb8f24e7a259db165677cd390947742d32439b8d
Zak MacDonald <me@zakmac.com>
2015-08-21 00:32:20 -0700

    Work in progress on #1, needs tests; Also contains minor refactor:
    - `decoratedDuration` and `decoratedSpeed` computed properties have been removed and their functionality has been moved into normalization methods.
    - `decoratedHref` has been renamed `normalizedHref` and is no longer a computed property.
    - `didinsertElement` functionality has moved to `init`.
    - Touch move events on mobile now cancel mutable scroll animations.

--- addon/components/deep-link.js
+++ addon/components/deep-link.js
@@ -15,74 +15,12 @@ export default Ember.Component.extend({
   // properties
 
   /**
-   * @access      public
    * @description bind properties to component's tag attributes in the DOM
    * @name        classNames
    * @type        {integer}
    */
-  attributeBindings: ['decoratedHref:href'],
+  attributeBindings: ['normalizedHref:href'],
   /**
-   * @access      private
-   * @description sanitized `this.duration`
-   * @name        decoratedDuration
-   * @returns     {integer} time in milliseconds to scroll
-   */
-  decoratedDuration: Ember.computed('duration', function() {
-
-    var duration = parseInt(this.get('duration'));
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-    if (typeof duration === 'number' && !isNaN(duration)) {
-
-      return duration;
-
-    } else {
-
-      return 0;
-    }
-  }),
-  /**
-   * @access      private
-   * @description sanitized `this.href`
-   * @name        decoratedHref
-   * @type        {Ember.computed}
-   * @returns     {string} the target anchor name, decorated with a '#'
-   */
-  decoratedHref: Ember.computed('href', function() {
-
-    var href = this.get('href');
-    var type = typeof href;
-
-    switch (type === 'string' || (type === 'number' && !isNaN(href))) {
-
-      case false: href = ''; break;
-    }
-
-    return '#' + href;
-  }),
-  /**
-   * @access      private
-   * @description sanitized `this.speed`
-   * @name        decoratedSpeed
-   * @type        {Ember.computed}
-   * @returns     {integer} pixels/second to scroll at
-   */
-  decoratedSpeed: Ember.computed('speed', function() {
-
-    var speed = parseInt(this.get('speed'));
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-    if (typeof speed === 'number' && !isNaN(speed)) {
-
-      return speed;
-
-    } else {
-
-      return 999999999;
-    }
-  }),
-  /**
-   * @access      public
    * @description scroll animation duration in milliseconds, superceded
    *   by `this.speed`
    * @name        duration
@@ -90,35 +28,36 @@ export default Ember.Component.extend({
    */
   duration: 0,
   /**
-   * @access      public
    * @description target id/name
    * @name        duration
    * @type        {string}
    */
   href: '',
   /**
-   * @access      public
    * @description whether scrollTo events can be cancelled/overridden
    * @name        duration
    * @type        {string}
    */
   mutable: true,
   /**
-   * @access      public
+   * @description normalized version of href, used to bind href dom attribute
+   * @name        normalizedHref
+   * @type        {integer}
+   */
+  normalizedHref: '',
+  /**
    * @description speed in pixels/seconds to scroll
    * @name        duration
    * @type        {integer}
    */
   speed: 999999999,
   /**
-   * @access      public
    * @description component's tag in the DOM
    * @name        tagName
    * @type        {string}
    */
   tagName: 'a',
   /**
-   * @access      public
    * @description text contained in the href, overridden by yielded content
    * @name        duration
    * @type        {string}
@@ -127,22 +66,31 @@ export default Ember.Component.extend({
 
   // methods
 
+  /**
+   * @description calculate how long a scroll event should take
+   * @name        calculateDuration
+   */
   calculateDuration: function(distance) {
 
-    distance = parseInt(distance);
+        distance = parseInt(distance);
+    var duration = this.normalizeDuration();
+    var speed = this.normalizeSpeed();
 
-    // if a speed was specified, return an appropriate duration to maintain
-    // that speed
-    if (this.get('decoratedSpeed') > 0 && !isNaN(distance)) {
+    // if a duration was specified
+    if (duration > 0) {
 
-      return Math.floor(distance / this.get('decoratedSpeed') * 1000);
+      return duration;
+
+    // if a speed was specified
+    } else if (speed > 0 && !isNaN(distance)) {
+
+      return Math.floor(distance / speed * 1000);
 
     } else {
 
       return 0;
     }
   },
-
   /**
    * @description runs the `cancelAnimation` method on DeepLinkService
    * @name        cancelAnimation
@@ -150,36 +98,6 @@ export default Ember.Component.extend({
   cancelAnimation: function() {
 
     this.get('deepLinkService').cancelAnimation(this.get('mutable'));
-  },
-  /**
-   * @description runs once the component element has been inserted into DOM
-   * @name        didInsertElement
-   */
-  didInsertElement: function() {
-
-    this._super();
-
-    var component = this;
-    var deepLinkService = this.get('deepLinkService');
-
-    // check if there is already a cancel event bound to scroll, prevents
-    //   duplicate bindings for mulitple components
-    // @todo: ensure one component being removed won't unbind scroll for other
-    //   DeepLinkComponents on the page
-    if (!deepLinkService.get('isCancelBound')) {
-
-      deepLinkService.set('isCancelBound', true);
-
-      // run a debounce'd cancelAnimation() on manual scroll events to avoid scrolljacking
-      Ember.$(window).bind('scroll mousedown DOMMouseScroll mousewheel keyup', function(e) {
-
-        // check for manual scroll/click interactions
-        if (e.which > 0 || e.type === 'mousedown' || e.type === 'mousewheel') {
-
-          Ember.run.throttle(component, component.cancelAnimation, 500);
-        }
-      });
-    }
   },
   /**
    * @description scroll to an anchor tag with the specified name attribute
@@ -194,7 +112,7 @@ export default Ember.Component.extend({
     var currentPosition = Ember.$('body').scrollTop();
     var distance = 0;
     var easing = this.get('easing');
-    var $target = Ember.$('a[name='+ this.get('href') +'], ' + this.get('decoratedHref'));
+    var $target = Ember.$('a[name="'+ this.get('href') +'"], #' + this.get('href'));
     // y position of the target element; defaults to current position, which is
     // used when no target is found
     var targetPosition = currentPosition;
@@ -202,8 +120,7 @@ export default Ember.Component.extend({
     // check for known jQuery property, for safety
     if ($target.selector && $target.length) {
 
-      targetPosition = $target.offset();
-      targetPosition = targetPosition.top;
+      targetPosition = $target.offset().top;
 
       // set the distance between here and there
       distance = Math.abs(currentPosition - targetPosition);
@@ -218,6 +135,95 @@ export default Ember.Component.extend({
           component.calculateDuration(distance),
           easing
         );
+    }
+  },
+  /**
+   * @description runs once the component element has been inserted into DOM
+   * @name        didInsertElement
+   */
+  init: function() {
+
+    this._super();
+
+    this.normalizeHref();
+
+    var component = this;
+    var deepLinkService = this.get('deepLinkService');
+
+
+    // check if there is already a cancel event bound to scroll. this prevents
+    //   duplicate bindings for mulitple components
+    // @todo:
+    // - look into a hash to manage unbinding
+    if (!deepLinkService.get('isCancelBound')) {
+
+      deepLinkService.set('isCancelBound', true);
+
+      // run a debounce'd cancelAnimation() on manual scroll events to avoid scrolljacking
+      Ember.$(window).bind('scroll mousedown DOMMouseScroll mousewheel keyup touchmove', function(e) {
+
+        // check for manual scroll/click interactions
+        if (e.which > 0 || e.type === 'mousedown' || e.type === 'mousewheel' || e.type === 'touchmove') {
+
+          Ember.run.throttle(component, component.cancelAnimation, 500);
+        }
+      });
+    }
+  },
+  /**
+   * @description normalize the value of duration, ensuring it's an integer
+   * @name        normalizeDuration
+   */
+  normalizeDuration: function() {
+
+    var duration = parseInt(this.get('duration'));
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+    if (typeof duration === 'number' && !isNaN(duration)) {
+
+      return duration;
+
+    } else {
+
+      return 0;
+    }
+  },
+  /**
+   * @description normalize the value of href, ensuring it's a string prepended
+   *   with a hash
+   * @name        normalizeHref
+   */
+  normalizeHref: function() {
+
+    var href = this.get('href');
+    var type = typeof href;
+
+    switch (type === 'string' || (type === 'number' && !isNaN(href))) {
+
+      case true: href = '#' + href; break;
+      default: href = '#';
+    }
+
+    this.set('normalizedHref', href);
+
+    return href;
+  },
+  /**
+   * @description normalize the value of speed, ensuring it's an integer
+   * @name        normalizeSpeed
+   */
+  normalizeSpeed: function() {
+
+    var speed = parseInt(this.get('speed'));
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+    if (typeof speed === 'number' && !isNaN(speed)) {
+
+      return speed;
+
+    } else {
+
+      return 999999999;
     }
   },
 
